818c5ef4dc114fe892f9f90f04ab4dfba3ba69f7
burgokmen <burgokmen@gmail.com>
2023-08-26 22:49:12 +0300

    axios needs reviewl, grid works perfect

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,4 @@
-//import { axios } from "axios";
+import { axios } from "axios";
 import React, { useState } from "react";
 
 // önerilen başlangıç stateleri
@@ -16,19 +16,22 @@ export default function AppFunctional(props) {
   const [steps, setSteps] = useState(initialSteps);
   const [index, setIndex] = useState(initialIndex);
 
-  function getXY(ind) {
+  function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
-    const xIndex = (ind % 3) + 1;
-    const yIndex = Math.floor(ind / 3) + 1;
+    const xIndex = (index % 3) + 1;
+    const yIndex = Math.floor(index / 3) + 1;
     return { xIndex, yIndex };
   }
+  /* time 0 x 1 y 1
+  time 1 x 2 y 1
+  time 2 x 3 y 1 */
 
   function getXYMesaj() {
     // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
     // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
     // tamamen oluşturulmuş stringi döndürür.
-    const { xIndex, yIndex } = getXY(index);
+    const { xIndex, yIndex } = getXY();
     const currentMessage = `Koordinatlar (${xIndex}, ${yIndex})`;
     return currentMessage;
   }
@@ -45,32 +48,67 @@ export default function AppFunctional(props) {
     // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
     // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
     // şu anki indeksi değiştirmemeli.
+    let newIndex = index;
+    let { xIndex, yIndex } = getXY();
+    if (yon === "left" && xIndex > 1) {
+      return (newIndex -= 1);
+    }
+    if (yon === "up" && yIndex > 1) {
+      return (newIndex -= 3);
+    }
+    if (yon === "right" && xIndex < 3) {
+      return (newIndex += 1);
+    }
+    if (yon === "down" && yIndex < 3) {
+      return (newIndex += 3);
+    }
+
+    return newIndex;
   }
 
   function ilerle(evt) {
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
-    setSteps(steps + 1);
+
+    const yon = evt.target.id;
+    const newIndex = sonrakiIndex(yon);
+    let currentSteps = index !== newIndex ? steps + 1 : steps;
+    if (index === newIndex) {
+      const yonMessage = `${yon} tarafa gidemezsin `;
+      setMessage(yonMessage);
+      setInterval(() => setMessage("Bru Grid iftiharla sunar"), 5000);
+      // Ask! Burda bi kac ilerlettikten sonra direk bu yaziya donuyor, bunu sor.
+    }
+    setSteps(currentSteps);
+    setIndex(newIndex);
   }
 
   function onChange(evt) {
     // inputun değerini güncellemek için bunu kullanabilirsiniz
-    setMessage(evt.target.value);
+    setEmail(evt.target.value);
   }
 
   function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
     evt.preventDefault();
     console.log("submit edildi");
+    let { xIndex, yIndex } = getXY();
+    const payload = {
+      xIndex: xIndex,
+      yIndex: yIndex,
+      email: email,
+      steps: steps,
+    };
 
-    /*     axios
-      .post("http://localhost:9000/api/result", {})
+    axios
+      .post("http://localhost:9000/api/result", payload)
       .then(function (response) {
-        console.log(response?.data);
+        console.dog(response.data);
+        setMessage(response.data.message);
       })
       .catch(function (error) {
         console.log(error);
-      }); */
+      });
   }
 
   return (
@@ -81,13 +119,13 @@ export default function AppFunctional(props) {
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
-          <div key={idx} className={`square${idx === 4 ? " active" : ""}`}>
-            {idx === 4 ? "B" : null}
+          <div key={idx} className={`square${idx === index ? " active" : ""}`}>
+            {idx === index ? "B" : null}
           </div>
         ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
         <button onClick={ilerle} id="left">
